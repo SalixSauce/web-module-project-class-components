1f7711946542a89208815fc58ab92d59b75e29d2
salix <csauce295@gmail.com>
2024-09-12 21:32:06 -0500

    project completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
+
+let id = 0;
+let getId = () => ++id
+
+const initialTodos = [
+  {id: getId(), name: 'Organize garage', completed: false},
+  {id: getId(), name: 'Bake Cookies', completed: true},
+  {id: getId(), name: 'Learn Class components', completed: false}
+]
 
 export default class App extends React.Component {
+  state = {
+    todos: initialTodos
+  }
+  addTodo = (name) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({id: getId(), completed: false, name  })
+    })
+  }
+
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td=>{
+      if(id == td.id ) return {...td, completed: !td.completed } 
+      return td
+      } )
+    })
+  }
   render() {
     return (
       <div>
-        Todo App
+       <TodoList  todos={this.state.todos} toggleCompletion={this.toggleCompletion}/>
+       <Form addTodo={this.addTodo}/>
       </div>
     )
   }
